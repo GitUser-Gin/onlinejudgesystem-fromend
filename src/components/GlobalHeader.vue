807edb9640e1ee32a0d038d17d933aed65d0e005
Gin <2894524580@qq.com>
2024-09-19 16:10:30 +0800

    init

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <a-row id="globalHeader" align="center" :wrap="false">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
+      >
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/logo.svg" alt="" />
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in visibleRoutes" :key="item.path">
+          {{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>
+        {{ store.state.user?.loginUser?.userName ?? "未登录" }}
+      </div>
+    </a-col>
+  </a-row>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+import checkAccess from "@/access/checkAccess";
+import ACCESS_ENUM from "@/access/accessEnum";
+
+const router = useRouter();
+const store = useStore();
+
+// 显示在菜单的路由数组
+const visibleRoutes = computed(() => {
+  return routes.filter((item, index) => {
+    if (item.meta?.hideInMenu) {
+      return false;
+    }
+    // 根据权限过滤菜单
+    if (
+      !checkAccess(store.state.user.loginUser, item?.meta?.access as string)
+    ) {
+      return false;
+    }
+    return true;
+  });
+});
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转后,更新选中的菜单项
+router.afterEach((to) => {
+  selectedKeys.value = [to.path];
+});
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "Admin_Adrian",
+    userRole: ACCESS_ENUM.ADMIN,
+  });
+}, 3000);
+</script>
+
+<style scoped>
+.title-bar {
+  height: 59px;
+}
+
+.logo {
+  height: 59px;
+}
+</style>
---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="userLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <a-space>
+          <img src="../assets/logo.svg" class="logo" alt="" />
+        </a-space>
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer"
+        >编程导航 by 程序员:Adrian
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#userLayout {
+  text-align: center;
+  background: url("https://gw.alipayobjects.com/zos/rmsportal/FfdJeJRQWjEeGTpqgBKj.png")
+    0 0 / 100% 100%;
+}
+
+#userLayout .header {
+  height: 75px;
+}
+
+#userLayout .content {
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+#userLayout .footer {
+  background-color: black;
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+  color: white;
+}
+</style>
+<script></script>
